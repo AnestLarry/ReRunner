a424be5760710b54d1409e0a33f840d3e8f5e83f
Anest <anest@66ws.cc>
2020-03-24 08:12:26 -0400

    add multi tasks mode

--- main.go
+++ main.go
@@ -11,16 +11,25 @@ import (
 )
 
 var (
-	RRSTemplate = map[string]interface{}{"WatchFiles": map[string]string{"filename1": "filehash", "filename2": "filehash"},
-		"RunCommands": [][]string{[]string{"cmd.exe", "/c", "cls"}, []string{"cmd.exe", "/c", "dir"}},
-		"ByLine":      false}
-
 	RRSettingsFileNameString = "./RRSetting.json"
 	TemplateBytes, _         = json.Marshal(RRSTemplate)
-	Version                  = "Feb 15,2020."
+	Version                  = "Mar 24,2020."
 	RRSettings               = map[string]interface{}{}
-	IsRun                    = false
 	wg                       sync.WaitGroup
+
+	RRSTemplate = map[string]interface{}{
+		"Tasks": []interface{}{
+			map[string]interface{}{
+				"WatchFiles":  map[string]string{"filename1": "filehash", "filename2": "filehash"},
+				"RunCommands": [][]string{[]string{"cmd.exe", "/c", "cls"}, []string{"cmd.exe", "/c", "dir"}},
+			},
+			map[string]interface{}{
+				"WatchFiles":  map[string]string{"filename3": "filehash", "filename4": "filehash"},
+				"RunCommands": [][]string{[]string{"cmd.exe", "/c", "cls"}, []string{"cmd.exe", "/c", "dir"}},
+			},
+		},
+		"ByLine":  false,
+		"Version": Version}
 )
 
 func Init() {
@@ -52,8 +61,10 @@ func Init() {
 		if err != nil {
 			panic(err)
 		}
-
 		json.Unmarshal(data, &RRSettings)
+		if RRSettings["Version"].(string) != Version {
+			panic("RRSettings.json doesn't feat current program version.")
+		}
 	} else {
 		ioutil.WriteFile(RRSettingsFileNameString, TemplateBytes, 0644)
 		os.Exit(0)
@@ -62,36 +73,34 @@ func Init() {
 }
 
 func CheckFileHash() {
-	for k, v := range RRSettings["WatchFiles"].(map[string]interface{}) {
-		if libs.LibsXExists(k) {
-
-			data := libs.LibsXSha1FileString(k)
-			if v != data {
-				RRSettings["WatchFiles"].(map[string]interface{})[k] = data
-				IsRun = true
+	for t_index, t := range RRSettings["Tasks"].([]interface{}) {
+		for k, v := range t.(map[string]interface{})["WatchFiles"].(map[string]interface{}) {
+			if libs.LibsXExists(k) {
+				data := libs.LibsXSha1FileString(k)
+				if v != data {
+					RRSettings["Tasks"].([]interface{})[t_index].(map[string]interface{})["WatchFiles"].(map[string]interface{})[k] = data
+					//RRSettings["WatchFiles"].(map[string]interface{})[k] = data
+					RunCommands(RRSettings["Tasks"].([]interface{})[t_index])
+					goto endcheck
+				}
 			}
 		}
 	}
-	time.Sleep(500 * time.Millisecond)
-
+endcheck:
 }
 
-func RunCommands() {
-	if IsRun {
-		libs.LibsXClear()
-		for _, v := range RRSettings["RunCommands"].([]interface{}) {
-			var cArgs []string
-			for _, value := range v.([]interface{}) {
-				cArgs = append(cArgs, value.(string))
-			}
-			if RRSettings["ByLine"].(bool) {
-				libs.LibsXexecCommand(cArgs[0], cArgs[1:])
-			} else {
-				libs.LibsXExecShell(cArgs[0], cArgs[1:])
-			}
+func RunCommands(task interface{}) {
+	libs.LibsXClear()
+	for _, v := range task.(map[string]interface{})["RunCommands"].([]interface{}) {
+		var cArgs []string
+		for _, value := range v.([]interface{}) {
+			cArgs = append(cArgs, value.(string))
 		}
-		SaveToRRSettings(RRSettings)
-		IsRun = false
+		if RRSettings["ByLine"].(bool) {
+			libs.LibsXexecCommand(cArgs[0], cArgs[1:])
+		} else {
+			libs.LibsXExecShell(cArgs[0], cArgs[1:])
+		}
 	}
 }
 
@@ -106,7 +115,8 @@ func SaveToRRSettings(v interface{}) {
 func Process() {
 	for {
 		CheckFileHash()
-		RunCommands()
+		SaveToRRSettings(RRSettings)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
